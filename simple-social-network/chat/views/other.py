deacee7b3e00bb7c3f3607a916a97fc15bc62f9d
Abolfazl <bateternali@gmail.com>
2021-12-22 01:14:58 +0330

    webterminal

--- simple-social-network/chat/views/other.py
+++ simple-social-network/chat/views/other.py
@@ -1,8 +1,7 @@
 from django.http import HttpResponseRedirect
-from proxy.views import proxy_view
 
 
 def other(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect("/login")
-    return proxy_view(request, "http://webterminal:80")
+    return HttpResponseRedirect('/landing')
