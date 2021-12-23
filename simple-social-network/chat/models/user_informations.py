2649ab122b4793f7dac186940192b7d3ccd32690
Abolfazl <bateternali@gmail.com>
2021-12-23 18:22:25 +0330

    rename some fields

--- simple-social-network/chat/models/user_informations.py
+++ simple-social-network/chat/models/user_informations.py
@@ -1,8 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from enum import Enum
 
 
 class UserInformations(models.Model):
+    class UserType(Enum):
+
+        STUDENT = "STUDENT"
+        TEACHER = "TEACHER"
+
+        @classmethod
+        def choices(cls):
+            return tuple((i.name, i.value) for i in cls)
+
     owner = models.ForeignKey(
         User, on_delete=models.CASCADE,
         related_name='owner', null=True, db_index=True)
@@ -13,6 +23,8 @@ class UserInformations(models.Model):
     profile_picture = models.CharField(max_length=100, null=True)
     bio = models.CharField(max_length=200, default='')
     verified = models.BooleanField(default=False)
+    user_type = models.CharField(
+        max_length=255, choices=UserType.choices(), null=True)
 
     class Meta:
         db_table = u'user_information'
