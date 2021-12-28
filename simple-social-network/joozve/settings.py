7c33e123f268500fcf38c2bff74f6a575dde64d0
Abolfazl <bateternali@gmail.com>
2021-12-29 01:52:59 +0330

    first api

--- simple-social-network/joozve/settings.py
+++ simple-social-network/joozve/settings.py
@@ -97,19 +97,19 @@ WSGI_APPLICATION = 'joozve.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': os.environ.get('SQL_DATABASE', ''),
-    #     'USER': os.environ.get('SQL_USER', ''),
-    #     'PASSWORD': os.environ.get('SQL_PASSWORD', ''),
-    #     'HOST': os.environ.get('SQL_HOST', ''),
-    #     'PORT': os.environ.get('SQL_PORT', ''),
-    #     'CONN_MAX_AGE': 0,
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('SQL_DATABASE', ''),
+        'USER': os.environ.get('SQL_USER', ''),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', ''),
+        'HOST': os.environ.get('SQL_HOST', ''),
+        'PORT': os.environ.get('SQL_PORT', ''),
+        'CONN_MAX_AGE': 0,
+    }
 }
 
 
